e63671529b92e3f309f5a8d9b740099320967543
Vicent Galiana <vgaliana@clearpeople.com>
2016-02-06 18:45:58 +0100

    Update assembly information

--- uCommerce ShowConfig/Website/Properties/AssemblyInfo.cs
+++ uCommerce ShowConfig/Website/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Website")]
+[assembly: AssemblyTitle("ClearPeople UCommerce ShowConfig Website")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Website")]
+[assembly: AssemblyCompany("ClearPeple")]
+[assembly: AssemblyProduct("ClearPeople UCommerce ShowConfig Website")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
