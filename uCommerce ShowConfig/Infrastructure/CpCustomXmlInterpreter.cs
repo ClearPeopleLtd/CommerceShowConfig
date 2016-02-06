ac1d9b2e3dd53e3b1c3e181fd0f73da3b7534166
Vicent Galiana <vgaliana@clearpeople.com>
2016-02-06 16:04:03 +0100

    INItial build to get the configuration

---
+++ uCommerce ShowConfig/Infrastructure/CpCustomXmlInterpreter.cs
@@ -0,0 +1,43 @@
+﻿using Castle.Core.Resource;
+using Castle.MicroKernel;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.MicroKernel.SubSystems.Resource;
+using Castle.Windsor.Configuration.Interpreters;
+using Castle.Windsor.Configuration.Interpreters.XmlProcessor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using UCommerce.Infrastructure.Components.Windsor;
+
+namespace ClearPeople.UCommerce.Infrastructure
+{
+    public class CpCustomXmlInterpreter: CustomXmlInterpreter
+    {
+        public CpCustomXmlInterpreter(string filename) : base(filename)
+        {
+        }
+
+        public CpCustomXmlInterpreter(IResource source) : base(source)
+        {
+        }
+        public XmlNode GetConfig(IResource source, IConfigurationStore store, IKernel kernel)
+        {
+            CustomXmlProcessor customXmlProcessor = new CustomXmlProcessor(base.EnvironmentName, kernel.GetSubSystem(SubSystemConstants.ResourceKey) as IResourceSubSystem);
+            try
+            {
+                XmlNode xmlNodes = customXmlProcessor.Process(source);
+                (new PartialComponentElementMerger()).MergePartialComponentsIntoComponents(xmlNodes);
+                return xmlNodes;
+
+
+            }
+            catch (XmlProcessorException xmlProcessorException)
+            {
+                throw new ConfigurationProcessingException("Unable to process xml resource.", xmlProcessorException);
+            }
+        }
+    }
+}
---
+++ uCommerce ShowConfig/Infrastructure/ExtendedWindsorContainer.cs
@@ -0,0 +1,46 @@
+﻿using Castle.Core.Resource;
+using Castle.MicroKernel;
+using Castle.MicroKernel.ModelBuilder;
+using Castle.MicroKernel.Resolvers.SpecializedResolvers;
+using Castle.MicroKernel.SubSystems.Conversion;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using ClearPeople.UCommerce.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ClearPeople.uCommerce.Infrastructure
+{
+    internal class CpExtendedWindsorContainer : WindsorContainer
+    {
+        private string XmlPath;
+        public CpExtendedWindsorContainer(string xmlFilePath) : base(CpExtendedWindsorContainer.CreateKernel(), new DefaultComponentInstaller())
+        {
+            if (xmlFilePath == null)
+            {
+                throw new ArgumentException("xmlFilePath");
+            }
+            XmlPath = xmlFilePath;
+            this.Kernel.Resolver.AddSubResolver(new ListResolver(this.Kernel));
+            this.Kernel.AddSubSystem(SubSystemConstants.ConversionManagerKey, new DefaultConversionManager());
+            
+        }
+        public XmlNode GetConfig()
+        {
+            FileResource fileResource = new FileResource(XmlPath);
+            return (new CpCustomXmlInterpreter(fileResource)).GetConfig(fileResource, this.Kernel.ConfigurationStore, this.Kernel);
+            
+        }
+
+        private static IKernel CreateKernel()
+        {
+            DefaultKernel defaultKernel = new DefaultKernel();
+            defaultKernel.ComponentModelBuilder = new DefaultComponentModelBuilder(defaultKernel);
+            return defaultKernel;
+        }
+    }
+}
